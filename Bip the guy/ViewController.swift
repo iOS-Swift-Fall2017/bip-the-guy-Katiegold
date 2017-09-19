0ee7940488b5441ef67e8ca22726001b2cff9d73
K Gold <KatieGold@ks-macbook-pro.bc.edu>
2017-09-19 12:35:05 -0400

    Initial Commit

---
+++ Bip the guy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bip the guy
+//
+//  Created by K Gold on 9/19/17.
+//  Copyright © 2017 Katie Gold. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
